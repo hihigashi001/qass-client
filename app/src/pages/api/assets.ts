c910d39f39c54d75d7abef40964c91fb72a18402
higashionna <higashionna@everlifegroup.com>
2022-12-12 09:23:12 +0900

    loading削除２

--- app/src/pages/api/assets.ts
+++ app/src/pages/api/assets.ts
@@ -24,7 +24,6 @@ function runMiddleware(
 }
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<IAsset[]>) => {
-  // await new Promise((resolve) => setTimeout(resolve, 3000))
   await runMiddleware(req, res, cors)
   res.status(200).json(assetListData)
 }
