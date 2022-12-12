867421e2895ef1d6c9e2eec2e09eeaf2f117ee89
higashionna <higashionna@everlifegroup.com>
2022-12-12 16:18:12 +0900

    add: admin page create

---
+++ app/src/pages/admin.tsx
@@ -0,0 +1,12 @@
+const Admin = () => {
+  return (
+    <div>
+      <div>ユーザ管理</div>
+      <div>備品管理</div>
+      <div>商品管理</div>
+      <div>貸出履歴</div>
+    </div>
+  )
+}
+
+export default Admin
